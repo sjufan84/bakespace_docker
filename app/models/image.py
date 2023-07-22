e2d9935bf3398eaf41dfa790afdf6ec482b5ba95
David Thomas <dave_thomas@enoughwebapp.com>
2023-07-22 14:47:15 -0500

    code style

--- app/models/image.py
+++ app/models/image.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
-from pydantic.fields import Field
+""" This module contains the Image class """
+from pydantic import BaseModel, Field
 
 # Create a model for the Image class
 class Image(BaseModel):
+    """ A class to represent an image """
     url: str = Field(description="The url of the generated image")
 
 # @TODO add fields for the image class to integrate with Bakespace API
