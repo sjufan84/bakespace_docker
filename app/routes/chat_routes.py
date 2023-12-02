a0acbac51c9cf87c5a91b7a7a15610d7423eb888
David Thomas <dave_thomas@enoughwebapp.com>
2023-12-01 19:19:17 -0600

    updated requirements, updated endpoints

--- app/routes/chat_routes.py
+++ app/routes/chat_routes.py
@@ -2,14 +2,14 @@
 from typing import List, Optional
 import logging
 import json
-import markdown
 from fastapi import APIRouter, File, UploadFile
 from pydantic import BaseModel
 from app.utils.assistant_utils import poll_run_status, get_assistant_id, upload_files
 from app.models.runs import (
-  CreateThreadRunRequest, CreateMessageRunRequest
+  CreateThreadRunRequest, CreateMessageRunRequest, CreateThreadRequest, GetChefResponse
 )
 from app.dependencies import get_openai_client  
+
 
 # Define a router object
 router = APIRouter()
@@ -22,67 +22,81 @@ class RecipeSpec(BaseModel):
 # Set the allowed chef types
 allowed_chef_types = ["home_cook", "pro_chef", "adventurous_chef", None]
 
-@router.post("/get_recipe_chef_response", response_description="""
-            The response from the chef as a message object and\
-            the chat_history as a list of messages.""",
-)
-async def get_recipe_chef_response():
-    """ Endpoint to get a response from the chef to the user's question about a recipe. """
+
+# Define a function to initialize the chatbot with context and an optional recipe
+# Create a route to initialize a general chat session
+@router.post("/initialize_general_chat", response_description=
+            "The thread id for the run to be added to, the session id, and the message content.",
+            summary="Initialize a general chat session.",
+            tags=["Chat Endpoints"],
+            responses={200: {"thread_id": "The thread id for the run to be added to.",
+            "session_id" : "The session id.", "message_content" : "The message content."}})
+async def initialize_general_chat(context : CreateThreadRequest):
+  """ Endpoint to initialize a general chat session. """
+  client = get_openai_client()
+  session_id = context.session_id
+  if context.serving_size:
+    message_content = context.message_content + " " + "Serving size: " + context.serving_size
+  else:
+    message_content = context.message_content
+
+  message_content = "The context for this chat thread is " + message_content
+  
+  message_thread = client.beta.threads.create(
+    messages=[
+      {
+        "role": "user",
+        "content": f"{message_content}",
+        "metadata": context.message_metadata  
+      },
+    ]
+  )
+  return {"thread_id" : message_thread.id, "session_id" : session_id, "message_content" : message_content}
+
   
 @router.post("/get_chef_response", response_description=
-            "The response from the chef as a message object and\
-            the chat_history as a list of messages.",
+            "The thread id for the run to be added to, the chef response, and the session id.",
             summary="Get a response from the chef to the user's question.",
             tags=["Chat Endpoints"],
-            responses={200: {"description": "OK"}})
-async def get_chef_response(message_content: str,
-  message_metadata: Optional[object] = None, thread_id: str = None, chef_type: str="home_cook",
-  serving_size: Optional[str] = None):
+            responses={200: {"thread_id": "The thread id for the run to be added to.",
+            "chef_response" : "The response from the chef", "session_id" : "The session id."}})
+async def get_chef_response(chef_response: GetChefResponse):
   """ Endpoint to get a response from the chatbot to a user's question. """
   client = get_openai_client()
-  if serving_size:
-    message_content = message_content + " " + "Serving size: " + serving_size
+  # Check to make sure that there is a session_id and a thread_id
+  if not chef_response.session_id:
+    raise ValueError("Session ID is required.")
+  if not chef_response.thread_id:
+    raise ValueError("Thread ID is required.")
+  # Get the assistant id based on the chef type
+  assistant_id = get_assistant_id(chef_response.chef_type)
+
+  if chef_response.serving_size:
+    message_content = chef_response.message_content + " " + "Serving size: " + chef_response.serving_size
   else:
-    message_content = message_content
-  if thread_id:
-    # Get the assistant id based on the chef type
-    assistant_id = get_assistant_id(chef_type)
+    message_content = chef_response.message_content
 
-    # Create and send the message
-    message = client.beta.threads.messages.create(
-        thread_id,
-        content=message_content,
-        role="user",
-        metadata=message_metadata,
-    )
-    # Log the message
-    logging.info(f"Message created: {message}")
+  # Create and send the message
+  message = client.beta.threads.messages.create(
+      chef_response.thread_id,
+      content=message_content,
+      role="user",
+      metadata=chef_response.message_metadata,
+  )
+  # Log the message
+  logging.info(f"Message created: {message}")
 
-    # Create the run
-    run = client.beta.threads.runs.create(
-        assistant_id=assistant_id,
-        thread_id=thread_id
-    )
-    # Poll the run status
-    response = poll_run_status(run_id=run.id, thread_id=run.thread_id)
+  # Create the run
+  run = client.beta.threads.runs.create(
+      assistant_id=assistant_id,
+      thread_id=chef_response.thread_id
+  )
+  # Poll the run status
+  response = poll_run_status(run_id=run.id, thread_id=run.thread_id)
 
-  else:
-    run = client.beta.threads.create_and_run(
-    assistant_id=get_assistant_id(chef_type),  
-    thread={
-      "messages": [
-          {
-            "role" : "user",
-            "content" : message_content, 
-            "metadata" : message_metadata
-      }]}   
-    )
-    # Poll the run status
-    response = poll_run_status(run_id=run.id, thread_id=run.thread_id)
+  return {"chef_response" : response["message"], "thread_id" : chef_response.thread_id, "session_id" : chef_response.session_id}
+
   
-  # Check to see if the response["message"] is markdown or not
-  
-  return {"chef_response" : response["message"], "thread_id" : response["thread_id"], "response_markdown" : markdown.markdown(response["message"])}
   
 @router.get("/view_chat_history", response_description="The chat history returned as a dictionary.",
             tags=["Chat Endpoints"])
